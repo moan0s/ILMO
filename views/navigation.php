539982d8e62d5278e82b525774e6fe06d360aadb
GerJuli <julian-samuel@gebuehr.net>
2018-11-10 09:00:10 +0100

    Changed standard view of books

--- views/navigation.php
+++ views/navigation.php
@@ -9,13 +9,13 @@ if ($_SESSION['admin'] ==1){ echo '
 <li><a href="index.php?ac=book_new">Neues Buch</a></li>
 <li><a href="index.php?ac=user_new">Neue*r Nutzer*in</a></li>
 <li><a href="index.php?ac=lend_new">Neue Ausleihe</a></li>
-<li><a href="index.php?ac=user_search">Benutzer*in suchen</a></li>';
+<li><a href="index.php?ac=user_search">Benutzer*in suchen</a></li>
+<li><a href="index.php?ac=book_show_itemized">Zeige Bücher einzeln</a></li>';
 }
 echo '
 <li><a href="index.php?ac=user_self">Mein Profil</a></li>
 <li><a href="index.php?ac=lend_self">Meine Ausleihen</a></li>
 <li><a href="index.php?ac=logo">Ausloggen</a></li>
-<li><a href="index.php?ac=book_show_plain">Zeige nur Bücher</a></li>
 </ul>';
 ?>
 
