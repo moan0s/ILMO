a4d3cb91343ac3b8862803c662b72de3aee9562f
GerJuli <julian-samuel@gebuehr.net>
2019-03-16 12:25:31 +0100

    Changed more views to language arrays

--- views/login_form.php
+++ views/login_form.php
@@ -1,16 +1,16 @@
 <?php
 $login = '
-<br><h1>'.PLEASE_LOG_IN.' :</h1><br>
+<br><h1>'.$lang['PLEASE_LOG_IN'].' :</h1><br>
 
 	<form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="post">
 			<input type = hidden name="ac" value = "strt"> 
 
-			<label for ="user_info">'.USER_ID_OR_EMAIL.': </label><br>
+			<label for ="user_info">'.$lang['USER_ID_OR_EMAIL'].': </label><br>
 			<input id="user_info" type="text" name="login_user_info"><br><br>
-			<label for ="pw"> '.PASSWORD.' </label><br>
+			<label for ="pw"> '.$lang['PASSWORD'].' </label><br>
 			<input id="pw" type="password" name="login_password"><br>
-			<input type="submit" value="'.BUTTON_SEND.'">
-			<input type="reset" value="'.BUTTON_RESET.'">	
+			<input type="submit" value="'.$lang['BUTTON_SEND'].'">
+			<input type="reset" value="'.$lang['BUTTON_RESET'].'">	
 </form>';
 echo $login;
 
--- views/material_form.php
+++ views/material_form.php
@@ -1,27 +1,27 @@
 <?php
 $form .= '<form action="'.$_SERVER["PHP_SELF"].'" method="post">
 	<input type = hidden name="ac" value = "material_save">
-	'.MATERIAL_ID.': <input type="text" name="material_ID" value="'; 
+	'.$lang['MATERIAL_ID'].': <input type="text" name="material_ID" value="'; 
 if(isset($this->aRow['material_ID'])){
 	$form .= $this->aRow['material_ID'];
 } 
 $form .= '"> <br>';
 
 if(!isset($this->aRow['material_ID'])){
-	$form .= NUMBER.': <input type=\"text\" name="number"> <br>';
+	$form .= $lang['NUMBER'].': <input type=\"text\" name="number"> <br>';
 		}
-$form .= NAME.' : <input type="text" name="name" value=';
+$form .= $lang['NAME'].' : <input type="text" name="name" value=';
 if(isset($this->aRow['name'])){
 	$form .= $this->aRow['name'];
 } 
 $form .= '><br>'.
-	LOCATION.': <input type="text" name="location" value=';
+	$lang['LOCATION'].': <input type="text" name="location" value=';
 if(isset($this->aRow['location'])){
 	$form .= $this->aRow['location'];
 }
 $form .= '> <br>
-		<input type="submit" value='.BUTTON_SEND.'>
-		<input type="reset" value='.BUTTON_RESET.'>	
+		<input type="submit" value='.$lang['BUTTON_SEND'].'>
+		<input type="reset" value='.$lang['BUTTON_RESET'].'>	
 </form>';
 echo $form;
 ?>
