a6debd7b8273f748afb0535b3947b52bd6df0ffb
GerJuli <julian-samuel@gebuehr.net>
2019-03-16 12:06:47 +0100

    Chenged more to language arrays

--- views/all_user.php
+++ views/all_user.php
@@ -2,20 +2,20 @@
 $table = "<table border='1'>";
 		$table .=
 		'<tr>
-		<th>'.USER_ID.'</th>
-		<th>'.SURNAME.'</th>
-		<th>'.FORENAME.'</th>
-		<th>'.EMAIL.'</th>
-		<th>'.UID.'</th>
+		<th>'.$lang['USER_ID'].'</th>
+		<th>'.$lang['SURNAME'].'</th>
+		<th>'.$lang['FORENAME'].'</th>
+		<th>'.$lang['EMAIL'].'</th>
+		<th>'.$lang['UID'].'</th>
 		';
 
 		
 if ($_SESSION['admin']==1) {
 	$table .= 
-		'<th>'.BUTTON_CHANGE.'</th>
-		<th>'.BUTTON_DELETE.'</th>';
+		'<th>'.$lang['BUTTON_CHANGE'].'</th>
+		<th>'.$lang['BUTTON_DELETE'].'</th>';
 	if (($_SESSION['admin']==1) or ($this->r_user_ID = $_SESSION['user_ID'])){
-		$table .= '<th>'.BUTTON_SHOW_LOANS.'</th>';
+		$table .= '<th>'.$lang['BUTTON_SHOW_LOANS'].'</th>';
 	}
 }
 $table .= '</tr>';
@@ -30,11 +30,11 @@ $table .= '</tr>';
 			<td>'.$aResult['UID'].'</td>';
 			if ($_SESSION['admin']==1){
 				$table .=
-				'<td> <a href="index.php?ac=user_change&user_ID='.$aResult['user_ID'].'" >'.BUTTON_CHANGE.' </<> </td>
-				<td> <a href="index.php?ac=user_delete&user_ID='.$aResult['user_ID'].'" >'.BUTTON_DELETE.' </<> </td>';
+				'<td> <a href="index.php?ac=user_change&user_ID='.$aResult['user_ID'].'" >'.$lang['BUTTON_CHANGE'].' </<> </td>
+				<td> <a href="index.php?ac=user_delete&user_ID='.$aResult['user_ID'].'" >'.$lang['BUTTON_DELETE'].' </<> </td>';
 				if (($_SESSION['admin']==1) or ($this->r_user_ID = $_SESSION['user_ID'])){
 					$table .='
-						<td> <a href="index.php?ac=loan_show&user_ID='.$aResult['user_ID'].'" > '.BUTTON_SHOW_LOANS.' </<> </td>';
+						<td> <a href="index.php?ac=loan_show&user_ID='.$aResult['user_ID'].'" > '.$lang['BUTTON_SHOW_LOANS'].' </<> </td>';
 				}
 			}
 
@@ -49,7 +49,7 @@ if ($_SESSION['admin']==1){
 	$form .='
 		<form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="post">
 		<input type = hidden name="ac" value = "user_new">
-		<input type="submit" value="'.BUTTON_ADD_USER.'">
+		<input type="submit" value="'.$lang['BUTTON_ADD_USER'].'">
 		</form>';
 	echo $form;
 }
--- views/book_form.php
+++ views/book_form.php
@@ -1,7 +1,7 @@
 <?php
 $form = '<form action="'.$_SERVER["PHP_SELF"].'" method="post">
 	<input type = hidden name="ac" value = "book_save">'.
-	BOOK_ID.': <input type="text" name="book_ID" value="';
+	$lang['BOOK_ID'].': <input type="text" name="book_ID" value="';
 if(isset($this->aRow['book_ID']))
 {
 	$form .= $this->aRow['book_ID'];
@@ -9,26 +9,26 @@ if(isset($this->aRow['book_ID']))
 $form .= '"> <br>';
 if(!isset($this->aRow['book_ID'])){		
 	$form .= 
-		NUMBER.': <input type="text" name="number"> <br>';
+		$lang['NUMBER'].': <input type="text" name="number"> <br>';
 }
-$form .= TITLE.': <input type="text" name="title" value="';
+$form .= $lang['TITLE'].': <input type="text" name="title" value="';
 if(isset($this->aRow['title'])){
 	$form .= $this->aRow['title'];
 } 
 $form .= '"><br>'.
-	AUTHOR.': <input type="text" name="author" value="';
+	$lang['AUTHOR'].': <input type="text" name="author" value="';
 if(isset($this->aRow['author'])){
 	$form .= $this->aRow['author'];
 } 
 $form .= '"> <br>';
-$form .= LOCATION.': <input type="text" name="location" value="'; 
+$form .= $lang['LOCATION'].': <input type="text" name="location" value="'; 
 if(isset($this->aRow['location'])){
 	$form .= $this->aRow['location'];
 }
 
 $form .= '"> <br>
-		<input type="submit" value="'.BUTTON_SEND.'">
-		<input type="reset" value="'.BUTTON_RESET.'">	
+		<input type="submit" value="'.$lang['BUTTON_SEND'].'">
+		<input type="reset" value="'.$lang['BUTTON_RESET'].'">	
 </form>';
 echo $form;
 ?>
