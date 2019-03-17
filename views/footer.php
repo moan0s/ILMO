3bbe68fa6e08c62ce62d9d1bbe0053ff602f4b18
GerJuli <julian-samuel@gebuehr.net>
2019-03-17 20:26:08 +0100

    Fixed mor language errors

--- views/footer.php
+++ views/footer.php
@@ -9,10 +9,10 @@ $footer ='
 	<span class="forum_logo"></span>
 	</a>
 	<span class="link_list">
-	<h1>'.$this->oLang->library_info['LINKS'].': </h1>
+	<h1>'.$this->oLang->texts['LINKS'].': </h1>
 	<ul>
-		<li><a  href="'.$this->oLang->library_info['CONTACT_LINK'].'" title="'.$this->oLang->library_info['CONTACT'].'" >'.$this->oLang->library_info['CONTACT'].'</a></li>
-		<li><a href="'.$this->oLang->library_info['PRIVACY_LINK'].'" title="'.$this->oLang->library_info['PRIVACY'].'">'.$this->oLang->library_info['PRIVACY'].'</a></li>
+		<li><a  href="'.$this->oLang->library_info['CONTACT_LINK'].'" title="'.$this->oLang->texts['CONTACT'].'" >'.$this->oLang->texts['CONTACT'].'</a></li>
+		<li><a href="'.$this->oLang->library_info['PRIVACY_LINK'].'" title="'.$this->oLang->texts['PRIVACY'].'">'.$this->oLang->texts['PRIVACY'].'</a></li>
 	</ul>
 	</span>
 </div>
