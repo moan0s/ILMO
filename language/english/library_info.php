1b01c92f8e2f499e20abe9bf7669dbf6d885c837
Julian-Samuel Gebühr <julian-samuel@gebuehr.net>
2019-08-19 19:22:07 +0200

    Added Footer links to english language pack

--- language/english/library_info.php
+++ language/english/library_info.php
@@ -1,16 +1,21 @@
 <?php
 
 $library_info = array(
-'LIBRARY_NAME' => 'Library "Lerninsel"',
-'LIBRARY_DESCRIPTION' => 'Library of the students of Medical Engineering',
+	'LIBRARY_NAME' => 'Library "Lerninsel"',
+	'LIBRARY_DESCRIPTION' => 'Library of the students of Medical Engineering',
 
+	'CONTACT_LINK' => 'https://www.fs-medtech.de/kontakt/',
+	'PRIVACY_LINK' => 'https://www.fs-medtech.de/datenschutz',
 
-'CONTACT_LINK' => 'https://www.fs-medtech.de/kontakt/',
-'PRIVACY_LINK' => 'https://www.fs-medtech.de/datenschutz',
+	'ADMIN_MAIL' => 'hiwi@fs-medtech.de',
+	'ADMIN_NAME' => 'Lerninsel Hiwis',
 
-'ADMIN_MAIL' => 'hiwi@fs-medtech.de',
-'ADMIN_NAME' => 'Lerninsel Hiwis',
+	'URL' => 'https://www.fs-medtech.de/bib/',
 
-'URL' => 'https://www.fs-medtech.de/bib/'
+	'FOOTER_LINK_1' => 'https://www.fs-medtech.de/',
+	'FOOTER_LINK_1_TITLE' => 'Website der Fachschaft Medizintechnik',
+	'FOOTER_LINK_2' => 'https://www.medtechs.de/',
+	'FOOTER_LINK_2_TITLE' => 'Forum Medizintechnik',
+
 );
 ?>
