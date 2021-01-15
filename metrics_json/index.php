63b580014794f9ee302df0bd1b9727852b537d86
moan0s <julian-samuel@gebuehr.net>
2021-01-15 11:43:09 +0100

    Add more precise metrics

--- metrics_json/index.php
+++ metrics_json/index.php
@@ -17,14 +17,13 @@ $oObject = new Data;
 
 $metrics = array();
 
-$counts = array('user', 'books', 'material', 'loan');
+$counts = array('user', 'loan');
 foreach ($counts as &$key) {
 	$q_num = "SELECT COUNT(*) FROM ".$key.";";
 	$result = $oObject->sql_statement($q_num);
-	while($row = mysqli_fetch_all($result)) {
-		$metrics[$key] = (int) $row[0][0];
-	}
+	$metrics[$key] = (int) mysqli_fetch_all($result)[0][0];
 }
+
 $q_loan_length = "SELECT DATEDIFF(return_date, pickup_date) FROM `loan` WHERE `returned` = 1";
 $result = $oObject->sql_statement($q_loan_length);
 $loan_lengths = array();
@@ -35,11 +34,28 @@ $metrics['loan_length_mean'] = array_sum($loan_lengths)/count($loan_lengths);
 
 $q_admins = "SELECT COUNT(*) FROM `user` WHERE `admin` = 1";
 $result = $oObject->sql_statement($q_admins);
-while ($row = mysqli_fetch_assoc($result)) {
-	$admins = (int) $row["COUNT(*)"];
-}
-
+$admins = (int) mysqli_fetch_all($result)[0][0];
 $metrics['admin_num'] = $admins;
+
+$q_lent = "SELECT COUNT(*) FROM `".TABLE_BOOKS."` WHERE `lent` = 1";
+$result_l = $oObject->sql_statement($q_lent);
+$num_lent = (int) mysqli_fetch_all($result_l)[0][0];
+$metrics['lent_books_num'] = $num_lent;
+$q_available = "SELECT COUNT(*) FROM `books` WHERE `lent` = 0";
+$result_a = $oObject->sql_statement($q_available);
+$num_avail = (int) mysqli_fetch_all($result_a)[0][0];
+$metrics['avail_books_num'] = $num_avail;
+
+$q_lent = "SELECT COUNT(*) FROM `material` WHERE `lent` = 1";
+$result_l = $oObject->sql_statement($q_lent);
+$num_lent = (int) mysqli_fetch_all($result_l)[0][0];
+$metrics['lent_material_num'] = $num_lent;
+$q_available = "SELECT COUNT(*) FROM `material` WHERE `lent` = 0";
+$result_a = $oObject->sql_statement($q_available);
+$num_avail = (int) mysqli_fetch_all($result_a)[0][0];
+$metrics['avail_material_num'] = $num_avail;
+
+
 //var_dump($metrics);
 $metrics_as_json = json_encode($metrics);
 echo($metrics_as_json);
