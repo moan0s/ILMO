cdf7ddf2a01b196af6dfd7abe9d0a917eb138af7
moan0s <julian-samuel@gebuehr.net>
2021-02-05 18:04:16 +0100

    Format

--- class/book.php
+++ class/book.php
@@ -6,7 +6,6 @@ class Book extends Data {
 		$aFields= array();
 		if((isset($this->r_book_ID)) and ($this->r_book_ID!= "")){$aFields["book_ID"] = $this->r_book_ID;}
 		if((isset($this->r_title)) and ($this->r_title= "")){$aFields["title" ]= $this->r_title;}
-		
 		$this->p_result = $this->select_rows(TABLE_BOOKS, $aFields);
 		while($aRow=mysqli_fetch_assoc($this->p_result)){
 			$aBook[$aRow['book_ID']] = $aRow;
@@ -29,13 +28,13 @@ class Book extends Data {
 		$aBook[$aRow['title']] = $aRow;
 	}
 	return $aBook;
-	}	
+	}
 	function save_book(){
 		$aFields = array(
 			'title' => $this->r_title,
 			'author' => $this->r_author,
 			'location' => $this->r_location,
-			'lent' => null		
+			'lent' => null
 		);
 		if ((isset($this->r_number)) and ($this->r_number>0)){
 			for ($i=0; $i<$this->r_number; $i++){
@@ -47,14 +46,14 @@ class Book extends Data {
 				}
 				$this->ID=$this->store_data(TABLE_BOOKS, $aFields, FALSE, FALSE);
 			}
-				
+
 		}
 		else{
 			$aFields['book_ID'] = $this->r_book_ID;
 			$this->ID=$this->store_data(TABLE_BOOKS, $aFields, 'book_ID',$this->r_book_ID);
 		}
-				
 	}
+
 	function delete_book(){
 		$aFields = array (
 			'book_ID' => $this->r_book_ID
@@ -63,7 +62,7 @@ class Book extends Data {
 	}
 	function return_book($book_ID){
 		$aFields = array(
-			'lent' => 0		
+			'lent' => 0
 		);
 
 		$this->id = $this->store_data(TABLE_BOOKS, $aFields, 'book_ID',$book_ID);
