df0a911f824da9b526951f4b5138b70c3bf7f93a
moan0s <julian-samuel@gebuehr.net>
2021-02-05 19:45:32 +0100

    Format

--- views/material_form.php
+++ views/material_form.php
@@ -1,10 +1,10 @@
 <?php
 $form .= '<form action="'.$_SERVER["PHP_SELF"].'" method="post">
 	<input type = hidden name="ac" value = "material_save">
-	'.$this->oLang->texts['MATERIAL_ID'].': <input type="text" name="material_ID" value="'; 
+	'.$this->oLang->texts['MATERIAL_ID'].': <input type="text" name="material_ID" value="';
 if(isset($this->aRow['material_ID'])){
 	$form .= $this->aRow['material_ID'];
-} 
+}
 $form .= '"> <br>';
 
 if(!isset($this->aRow['material_ID'])){
@@ -13,7 +13,7 @@ if(!isset($this->aRow['material_ID'])){
 $form .= $this->oLang->texts['NAME'].' : <input type="text" name="name" value=';
 if(isset($this->aRow['name'])){
 	$form .= $this->aRow['name'];
-} 
+}
 $form .= '><br>'.
 	$this->oLang->texts['LOCATION'].': <input type="text" name="location" value=';
 if(isset($this->aRow['location'])){
@@ -21,7 +21,7 @@ if(isset($this->aRow['location'])){
 }
 $form .= '> <br>
 		<input type="submit" value='.$this->oLang->texts['BUTTON_SEND'].'>
-		<input type="reset" value='.$this->oLang->texts['BUTTON_RESET'].'>	
+		<input type="reset" value='.$this->oLang->texts['BUTTON_RESET'].'>
 </form>';
 echo $form;
 ?>
