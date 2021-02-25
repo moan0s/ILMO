be1a15ccd0b9817863a3e290a61f2f8eb16a66d1
moan0s <julian-samuel@gebuehr.net>
2021-02-26 00:29:44 +0100

    Adapt class structure to metrics

--- metrics_json/index.php
+++ metrics_json/index.php
@@ -8,27 +8,28 @@ session_start();
 header("Content-Type: application/json; charset=UTF-8");
 
 //start: includes
-include ("../config/config.inc.php");
-ini_set("display_errors", 0);
+include("../config/config.inc.php");
+ini_set("display_errors", 1);
 
-include (MODULE_PATH."class/class.php");
+include(MODULE_PATH."class/class.php");
 
-$oObject = new Data;
+$oLang = new Lang;
+$oObject = new Data($oLang);
 
 $metrics = array();
 
 $counts = array('user', 'loan');
 foreach ($counts as &$key) {
-	$q_num = "SELECT COUNT(*) FROM ".$key.";";
-	$result = $oObject->sql_statement($q_num);
-	$metrics[$key] = (int) mysqli_fetch_all($result)[0][0];
+    $q_num = "SELECT COUNT(*) FROM ".$key.";";
+    $result = $oObject->sql_statement($q_num);
+    $metrics[$key] = (int) mysqli_fetch_all($result)[0][0];
 }
 
 $q_loan_length = "SELECT DATEDIFF(return_date, pickup_date) FROM `loan` WHERE `returned` = 1";
 $result = $oObject->sql_statement($q_loan_length);
 $loan_lengths = array();
 while ($row = mysqli_fetch_assoc($result)) {
-	$loan_lengths[] = (int) $row["DATEDIFF(return_date, pickup_date)"];
+    $loan_lengths[] = (int) $row["DATEDIFF(return_date, pickup_date)"];
 }
 $metrics['loan_length_mean'] = array_sum($loan_lengths)/count($loan_lengths);
 
