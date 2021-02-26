8c1835ee86c8a3e382aad1e5ccf5de1510b689e1
moan0s <julian-samuel@gebuehr.net>
2021-02-26 22:55:46 +0100

    Smooth language change
    Without replacement of the language object the first page shown after
    the language change was still in the previous language. This is fixed
    with this change. Also use lang variable to show new language

--- views/changed_language.php
+++ views/changed_language.php
@@ -1,3 +1,2 @@
 <?php
-echo '<h1>'.$this->oLang->texts['CHANGED_LANGUAGE_TO'].': '.$_SESSION['language'].'</h1>';
-?>
+echo '<h1>'.$this->oLang->texts['CHANGED_LANGUAGE_TO'].': '.$this->oLang->texts[strtoupper($_SESSION['language'])].'</h1>';
