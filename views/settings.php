dc4740d207a617c9ee49d5d8bf0b6c892a886c73
moan0s <julian-samuel@gebuehr.net>
2021-04-16 17:50:28 +0200

    Add function to recursively generate form

--- views/settings.php
+++ views/settings.php
@@ -1,8 +1,51 @@
 <?php
 
+function text_input($label, $id, $value ="")
+{
+    /* Generates an input field with label
+     */
+    $output = "<div class=input_label_container><label for='$id'>$label</label>
+					<input type=text id='$id' name='$id' value='$value'></div>";
+    return $output;
+}
+
+
+function gen_form_inputs($texts, $array, $output = "", $prefix = "")
+{
+    /* Generate a form from an array of adjustable values
+     *
+     * $texts: array
+     * $array: array
+     * 		Array to be turned into form
+     * $output: String
+     * 		Output form
+     * $prefix: String
+     * 		Sting that will be added to the form ID
+     *
+     * Returns output form for array
+     */
+    foreach ($array as $key=>$val) {
+        if (is_string($val)) {
+            $id = $prefix.$key;
+            $key_in_texts = strtoupper($key);
+            if (array_key_exists($key_in_texts, $texts)) {
+                $label = $texts[$key_in_texts];
+            } else {
+                $label = $id;
+            }
+            $value = $val;
+            $output .= text_input($label, $id, $value);
+        } elseif (is_array($val)) {
+            $prefix = $prefix.$key."_";
+            $output = gen_form_inputs($texts, $val, $output, $prefix);
+        }
+    }
+    return $output;
+}
+
 $form = '<form action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'" method="post">';
-$form .='
-<input type = hidden name="ac" value = "settings_save">';
+$form .='<input type = hidden name="ac" value = "settings_save">';
+/*
 foreach ($this->settings['lang'] as $lang=>$aTexts) {
     $form.="<p><strong>".$lang."</strong><br>";
     foreach ($aTexts as $key=>$val) {
@@ -10,17 +53,9 @@ foreach ($this->settings['lang'] as $lang=>$aTexts) {
     }
     echo"</p>";
 }
+ */
 
-foreach ($this->settings as $key=>$val) {
-    if (is_string($val)) {
-        $form .= "<div class=input_label_container><label for=$key>".$this->oLang->texts[strtoupper($key)]."</label>
-		<input type=text id=$key name=$key value=$val></div>";
-    }
-}
+$form .= gen_form_inputs($this->oLang->texts, $this->settings);
 
-
-$form .= '
-<input type="submit" value="'.$this->oLang->texts['BUTTON_SEND'].'">
-</form>';
+$form .= '<input type="submit" value="'.$this->oLang->texts['BUTTON_SEND'].'"></form>';
 echo $form;
-?>
