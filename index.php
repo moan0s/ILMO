99d11982d4909bfa3af07847a1cf52f3be4583be
Sebastian1515 <mail@sebastian-ludwig.name>
2021-08-19 22:38:49 +0200

    Correct the password for the hashing function

--- index.php
+++ index.php
@@ -303,9 +303,8 @@ switch ($action) {
             if ($oData->payload["new_password"] == $oData->payload["confirm_password"]) {
                 $oUser = new User($oData);
                 $aUser['user_ID'] = $user_ID;
-		        $aUser['password'] = "";
-		        $oData = new Data($oLang);
-		        $aUser['password_hash'] = $oData->hash_password($sPassword);
+		$aUser['password'] = "";
+		$aUser['password_hash'] = $oData->hash_password($oData->payload['new_password']);
                 $oUser->save_user($aUser);
 
                 // If a token is used to change the password we mark the token as used
