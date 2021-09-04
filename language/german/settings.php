c431cb6cfd266d719b97fddc676e17443bab9782
moan0s <julian-samuel@gebuehr.net>
2021-09-04 09:24:14 +0200

    Add language strings

---
+++ language/german/settings.php
@@ -0,0 +1,16 @@
+<?php
+if (empty($lang) || !is_array($lang)) {
+    $lang = array();
+}
+
+//main texts that will be shown
+$lang = array_merge($lang, array(
+    "DEFAULT_LANGUAGE" => "Standard-Sprache",
+    "LOG_MAIL" => "Mails loggen? (0 für Nein, 1 für Ja)",
+    "MAIL_REMINDER_INTERVAL" => "Interval für E-Mail-Erinnerungen (in Tagen)",
+    "MAX_LOAN_TIME" => "Maximale Ausleihzeit (in Tagen, 0 für unbegrenzt)",
+    "MINIMUM_PW_LENGTH" => "Minimale Passwordlänge",
+    "MINIMUM_TOKEN_HOURS" => "Cooldown für Passwortreset-Tokengenerierung (in Stunden)",
+    "PATH_MAIL_LOG" => "Pfad für das Mail-Log",
+    "TIMEZONE" => "Zeitzone im Format Europe/Berlin"
+));
